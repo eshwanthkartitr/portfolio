8ecf04cfb68b5f0ccf60228093cee813464f650f
eshwanthkartitr <eshwankartitr@gmail.com>
2024-02-13 16:49:27 +0530

    Third Commit

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,30 +1,24 @@
 import React from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import Header from "./Header/Header.jsx";
 import Card from "./Card/Card.jsx";
-import { text } from "@fortawesome/fontawesome-svg-core";
+import My_img from "./assets/self_img.jpg";
+import "./App.css";
 
 function App() {
   return (
     <>
       <Header></Header>
       <Card
-        id="car-1"
         x={161}
-        y={304}
-        text="Hi i am a b.tech student from Amrita Vishwa Vidyapeetam and i am currently interested in ai related stuffs"
+        y={400}
+        text="Hi i am a b.tech student from Amrita Vishwa Vidyapeetam and i am currently interested in AI related stuffs"
         color="white"
+        bgcolor="#808080"
         font_size={19}
         font_family="Port Lligat Sans"
-      >
-        {/* Nested Card */}
-        <Card
-          styles={{ backgroundColor: "lightblue", padding: "20px", borderRadius: "20px" }}
-        >
-          <img src={reactLogo} alt="" />
-        </Card>
-      </Card>
+        img={My_img}
+        className="translatedCard"
+      ></Card>
     </>
   );
 }
--- Portfolio/src/Card/Card.jsx
+++ Portfolio/src/Card/Card.jsx
@@ -1,37 +1,78 @@
 import React from "react";
+import styles from "./Card.module.css";
 
-function Card({ x, y, text, color, font_size, font_family, styles, children }) {
+function Card({
+  x,
+  y,
+  text,
+  color,
+  bgcolor,
+  font_size,
+  font_family,
+  img,
+  className,
+}) {
   const cardStyle = {
     color: color,
     fontSize: font_size + "px",
     fontFamily: font_family,
     width: x + "px",
     height: y + "px",
-    backgroundColor: "lightblue",
+    backgroundColor: bgcolor,
     display: "flex",
     flexDirection: "column", // Set flex direction to column
     justifyContent: "flex-start", // Align items at the start
     alignItems: "center",
     position: "absolute",
-    left: "50%",
-    top: "50%",
-    transform: "translate(-50%, -50%)",
+    top: "55%",
+    transform: "translate(50%,-50%) ",
     borderRadius: "40px",
     padding: "40px",
+    textAlign: "center",
+    transition: "background-color 0.3s ease",
+    fontWeight: "normal",
+    lineHeight: "1.5",
   };
 
-  const imageContainerStyle = {
-    marginBottom: "20px", // Add margin to separate text and image
-    position: "relative", // Set position to relative
-    transform: "translateY(-100px)", // Move the child element up by 100px
+  const imageStyle = {
+    // Add margin to separate text and image
+    borderRadius: "30px",
+    objectFit: "cover",
+    width: "150px",
+    height: "150px",
+    backgroundColor: "#A9A9A9",
+    transition: "transform 0.3s ease",
+  };
+  const i1 = {
+    marginBottom: "15px",
+    backgroundColor: "#A9A9A9",
+    maxHeight: "150px",
+    borderRadius: "40px",
+  };
+
+  const handleHover = (e) => {
+    e.target.style.transform =
+      "translateY(-20px) translateX(100px) rotateZ(30deg)";
+  };
+
+  const handleLeave = (e) => {
+    e.target.style.transform = "translateY(0px) ";
   };
 
   return (
     <div style={cardStyle}>
-      {text}
-      <div style={imageContainerStyle}>
-        {children} {/* Render image */}
+      <div style={i1}>
+        <img
+          src={img}
+          alt="User"
+          style={imageStyle}
+          onMouseEnter={handleHover}
+          onMouseLeave={handleLeave}
+          className="image"
+        />
       </div>
+      <span>This is Eshwanth Karti T R</span>
+      {text}
     </div>
   );
 }
